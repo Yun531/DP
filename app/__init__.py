96d07661d159f47aa3abd44af5341ce14fd8ffe7
janghyunjun <buzz03312@naver.com>
2025-05-17 19:18:54 +0900

    chore: 초기화 코드 수정

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from .extensions import init_extensions
-from .domains import register_domains
+from .controllers import bp as papers_bp  # controllers/__init__.py에서 bp를 export
 
 def create_app() -> Flask:
     app = Flask(__name__)
     init_extensions(app)
-    register_domains(app)               # papers 도메인의 Blueprint 등록
+    app.register_blueprint(papers_bp)  # 이미 url_prefix가 설정돼 있음
     return app
